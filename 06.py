ba1e1a66cf95e4811d8942fa4fed0f5cb1743421
Josh Black <joshtheblack@gmail.com>
2022-12-06 00:31:43 -0500

    cleaner|

--- 06.py
+++ 06.py
@@ -5,8 +5,7 @@ class Puzzle:
         '''Pull in input data and create an array from the data'''
         with open(fileName) as file:
             lines = file.read()
-            groups = lines.split("\n")
-        return [x for x in groups]
+        return lines
 
     def __init__(self, file):
         self.data = self.importData(file)
@@ -15,18 +14,16 @@ class Puzzle:
     @timed
     def runA(self):
         '''Find index of the last character of first instance of 4 distinct consecutive characters'''
-        x = self.data[0]
-        for i in range(len(self.data[0])):
-            y = set(f"{x[i:i+4]}")
+        for i in range(len(self.data)):
+            y = set(f"{self.data[i:i+4]}")
             if len(y) == 4: break
         return i+4
 
     @timed
     def runB(self):
         '''Find index of the last character of first instance of 14 distinct consecutive characters'''
-        x = self.data[0]
-        for i in range(len(self.data[0])):
-            y = set(f"{x[i:i+14]}")
+        for i in range(len(self.data)):
+            y = set(f"{self.data[i:i+14]}")
             if len(y) == 14: break
         return i+14
 
@@ -38,4 +35,4 @@ class Puzzle:
 
 if __name__ == "__main__":
     day06 = Puzzle('06.in')
-    day06.run() # A: 1.5, B: 11, T: 13.9 
+    day06.run() # A: 0.9, B: 4.0, T: 6.2 
