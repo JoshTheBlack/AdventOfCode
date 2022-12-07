7bd73740e6a2d27283fede4c429fab72a4746692
Josh Black <joshtheblack@gmail.com>
2022-12-07 11:47:53 -0500

    Cleaning up

--- 07.py
+++ 07.py
@@ -1,5 +1,5 @@
 from collections import defaultdict as dd
-import pathlib
+from pathlib import Path
 from comm import timed
 
 def importData(fileName):
@@ -10,13 +10,13 @@ def importData(fileName):
 
 @timed
 def run(inputdata, d = False):
-    fs, cwd = dd(int), pathlib.Path("/")
+    fs, cwd = dd(int), Path("/")
     for line in inputdata:
         if line.startswith("$"):
             _, cmd, *args = line.split()
             if cmd == "cd":
                 if args[0] == "/":
-                    cwd = pathlib.Path("/")
+                    cwd = Path("/")
                 elif args[0] == "..":
                     cwd = cwd.parent
                 else:
@@ -25,17 +25,16 @@ def run(inputdata, d = False):
             pass
         else:
             _cwd = cwd / line.split()[1]
-            while _cwd != pathlib.Path("/"):
+            while _cwd != Path("/"):
                 _cwd = _cwd.parent
                 fs[_cwd] += int(line.split()[0])
     
     if not d:
         return sum(size for size in fs.values() if size <= 100_000)
     else:
-        return min(size for size in fs.values() if size >= fs.get(pathlib.Path("/")) - 40_000_000)
+        return min(size for size in fs.values() if size >= fs.get(Path("/")) - 40_000_000)
 
 if __name__ == '__main__':
     inputdata = importData('07.in')
     print(f"Part 1: {run(inputdata)}")
     print(f"Part 2: {run(inputdata, 1)}")
-# This one stumped me.  This is modified code from reddit.
