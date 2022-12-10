449bd9a2b1e4a2e911eec74dabe6176ee2394258
Josh Black <joshtheblack@gmail.com>
2022-12-10 01:19:13 -0500

    streamlined

--- 10.py
+++ 10.py
@@ -1,62 +1,47 @@
 from comm import timed
 
-class stacks:
-    def importData(self, fileName):
-        '''Pull in input data and create an array from the data'''
-        with open(fileName) as file:
-            lines = file.read()
-            groups = lines.split("\n")
-            data = []
-            for line in groups:
-                if line == 'noop':
-                    data.append(['noop',0])
-                    continue
-                x,y = line.split()
-                data.append([x,int(y)])
-        return data
 
-    def __init__(self, file):
-        self.data = self.importData(file)
-        self.cycles = [20,60,100,140,180,220]
-        return
+def importData(fileName):
+    '''Pull in input data and create an array from the data'''
+    with open(fileName) as file:
+        lines = file.read()
+        groups = lines.split("\n")
+        data = []
+        for line in groups:
+            if line == 'noop':
+                data.append(['noop',0])
+                continue
+            x,y = line.split()
+            data.append([x,int(y)])
+    return data
 
+@timed
+def run(data):
+    clock = 0
+    x = 1
+    total = 0
 
-    @timed
-    def run(self):
-        clock = 0
-        x = 1
-        signal = 0
-        display = []
-        for op, am in self.data:
-            display.append('#') if (abs(x - (clock % 40)) <= 1) else display.append('.')
-            clock += 1
-            signal += self.check(clock, x, self.cycles)
-            if op == "addx":
-                display.append('#') if (abs(x - (clock % 40)) <= 1) else display.append('.')
-                clock += 1
-                signal += self.check(clock, x, self.cycles)
+    def cycle():
+        nonlocal clock, total, x
+        clock += 1
+        if clock in {20,60,100,140,180,220}:
+            total += clock * x  
+        print("x " if abs(clock % 40 - x - 1) <= 1 else ". ", end="")
+        if clock % 40 == 0: print()
+
+    for op, am in data:
+        match op:
+            case "noop":
+                cycle()
+            case "addx":
+                cycle()
+                cycle()
                 x += am
-        d = [display[:40], display[40:80], display[80:120], display[120:160], display[160:200], display[200:240]]
-        self.printdisplay(d)
-        return signal
-
-    def printdisplay(self, d):
-        print(*d[0])
-        print(*d[1])
-        print(*d[2])
-        print(*d[3])
-        print(*d[4])
-        print(*d[5])
-        return
-
-    def check(self, clock, x, cycles):
-        if clock in cycles:
-            return clock * x
-        else:
-            return 0
+    
+    return total
 
 
 if __name__ == "__main__":
-    puzzle = stacks('10.in')
-    print(f"Part One: {puzzle.run()}") # T: 22  ms 
-    
+    d = importData('10.in')
+    print(f"Part One: {run(d)}") # T: 22  ms 
+
